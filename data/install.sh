40c29cdb7a7ecd97a23f6c936c9ccacca6a8111b
Mircea Silviu Itu <decemvre@gmail.com>
2016-02-25 15:07:29 +0200

    conf for nginx

--- data/install.sh
+++ data/install.sh
@@ -2,13 +2,14 @@
 
 echo "Running install.sh for $1"
 
-# TODO: Download selected version via  (from docker-sho-manager) from githup.com/prestashop/prestashop and run installer
-
-echo "Cloning prestashop repository"
-git clone --branch $1 --depth 1 https://2245dfb67d208f3503081b38814bdd54e04368f7:x-oauth-basic@github.com/OXID-eSales/oxideshop_ce.git /www
+echo "Cloning oxid4 repo"
+git clone --branch $1 --depth 1 https://github.com/OXID-eSales/oxideshop_ce.git /www
 
 echo "Installing composer packages"
-cd /www/source && composer.phar self-update && composer.phar config -g github-oauth.github.com 2245dfb67d208f3503081b38814bdd54e04368f7 && composer.phar install --ignore-platform-reqs
+cd  /www/source \
+    && composer.phar self-update \
+    && composer.phar install --ignore-platform-reqs
+
 
 echo "Setting filesystem permissions"
 chown -R www-data /www
